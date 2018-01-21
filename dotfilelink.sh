36fddd1eec112a239bd3eb4ae12fce02efb6d46c
Takaaki Kurihara <32270422+kuritaka@users.noreply.github.com>
2018-01-22 01:32:16 +0900

    change from ln command to mv command

--- dotfilelink.sh
+++ dotfilelink.sh
@@ -2,9 +2,9 @@
  
  
 FILE=`ls -A |egrep -v ".git|dotfilelink.sh"`
-cd ~/
+
 for i in $FILE
 do
     test -f ~/${i} && cp ~/${i}  ~/${i}.`date -d '1day ago' +%Y%m%d`
-    ln -s ~/dotfiles/${i} ~/${i}
+    mv ${i} ~/${i}
 done
